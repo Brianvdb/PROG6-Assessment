6c04db56f3e3261e3c5b4c3517328cd3a0bfe8de
Brian <bcf.vandenbroek@student.avans.nl>
2015-01-09 15:27:12 +0100

    winkelwagen exporteren naar pdf

--- PROG6-Assessment/ViewModel/AppieViewModel.cs
+++ PROG6-Assessment/ViewModel/AppieViewModel.cs
@@ -2,10 +2,13 @@ using DomainModel.Interfaces;
 using DomainModel.Model;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 
@@ -57,6 +60,7 @@ namespace PROG6_Assessment.ViewModel
         public ICommand OpenRecipeAddCommand { get; set; }
         public ICommand AddRecipeToShoppingListCommand { get; set; }
         public ICommand RemoveRecipeCommand { get; set; }
+        public ICommand SaveReceiptCommand { get; set; }
 
         // view models
         private ProductTypeVM _productType;
@@ -196,6 +200,7 @@ namespace PROG6_Assessment.ViewModel
             this.OpenRecipeAddCommand = new RelayCommand(OpenRecipeAddWindow);
             this.AddRecipeToShoppingListCommand = new RelayCommand(AddRecipeToShoppingList);
             this.RemoveRecipeCommand = new RelayCommand(RemoveRecipe);
+            this.SaveReceiptCommand = new RelayCommand(SaveReceipt);
 
 
             // observable collections
@@ -244,6 +249,140 @@ namespace PROG6_Assessment.ViewModel
             };
 
             this.productRepository.Add(product);*/
+        }
+
+        private void SaveReceipt()
+        {
+            if (ProductShoppingVMList.Count == 0)
+            {
+                string message = "Er staan geen producten in uw boodschappenlijstje.";
+                string caption = "Fout";
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show(message, caption, buttons, icon);
+                return;
+            }
+
+            try
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "Bonnetje"; // Default file name
+                dlg.DefaultExt = ".pdf"; // Default file extension
+                dlg.Filter = "PDF File (.pdf)|*.pdf"; // Filter files by extension
+
+                // Show save file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+
+                string filename = "";
+
+                // Process save file dialog box results
+                if (result == true)
+                {
+                    // Save document
+                    filename = dlg.FileName;
+                }
+                else
+                {
+                    return;
+                }
+
+                System.IO.FileStream fs = new FileStream(filename, FileMode.Create);
+                Document document = new Document(new Rectangle(200f, 500f), 10, 10, 10, 10);
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+
+                document.Open();
+
+                Font font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16);
+                Paragraph paragraph = new Paragraph("Appie Bonnetje", font);
+                paragraph.Alignment = Element.ALIGN_CENTER;
+                document.Add(paragraph);
+
+                font = FontFactory.GetFont(FontFactory.HELVETICA, 9);
+                paragraph = new Paragraph("Onderwijsboulevard 256", font);
+                paragraph.Alignment = Element.ALIGN_CENTER;
+                document.Add(paragraph);
+
+                paragraph = new Paragraph("5223 DJ 's-Hertogenbosch", font);
+                paragraph.Alignment = Element.ALIGN_CENTER;
+                document.Add(paragraph);
+
+                document.Add(new Paragraph(" "));
+                document.Add(new Paragraph("------------------------------------------------------------", font));
+
+                PdfPTable table = new PdfPTable(2);
+                table.WidthPercentage = 100;
+                table.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                foreach (ProductVM product in ProductShoppingVMList)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(product.Amount + " x " + product.ProductName, font));
+                    cell.Border = Rectangle.NO_BORDER;
+                    cell.HorizontalAlignment = Element.ALIGN_LEFT;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(product.Price.ToString(), font));
+                    cell.Border = Rectangle.NO_BORDER;
+                    cell.HorizontalAlignment = Element.ALIGN_RIGHT;
+                    table.AddCell(cell);
+                }
+
+                document.Add(table);
+
+                document.Add(new Paragraph("------------------------------------------------------------", font));
+
+                table = new PdfPTable(2);
+                table.WidthPercentage = 100;
+                table.HorizontalAlignment = Element.ALIGN_CENTER;
+                font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10);
+
+                PdfPCell tableCell = new PdfPCell(new Phrase("Totaal", font));
+                tableCell.Border = Rectangle.NO_BORDER;
+                tableCell.HorizontalAlignment = Element.ALIGN_LEFT;
+                table.AddCell(tableCell);
+
+                tableCell = new PdfPCell(new Phrase("€" + ShoppingListPrice, font));
+                tableCell.Border = Rectangle.NO_BORDER;
+                tableCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+                table.AddCell(tableCell);
+
+                font = FontFactory.GetFont(FontFactory.HELVETICA, 7);
+
+                document.Add(table);
+
+                paragraph = new Paragraph(DateTime.Now.ToString() + " ", font);
+                paragraph.Alignment = Element.ALIGN_RIGHT;
+                document.Add(paragraph);
+
+                document.Add(new Paragraph(" "));
+
+                font = FontFactory.GetFont(FontFactory.HELVETICA, 9);
+
+                paragraph = new Paragraph("Bedankt voor uw bestelling en graag tot ziens!", font);
+                paragraph.Alignment = Element.ALIGN_CENTER;
+                document.Add(paragraph);
+
+                paragraph = new Paragraph("www.ah.nl", font);
+                paragraph.Alignment = Element.ALIGN_CENTER;
+                document.Add(paragraph);
+
+                document.Close();
+                writer.Close();
+                fs.Close();
+
+                string message = "Uw winkelwagentje is succesvol opgeslagen.";
+                string caption = "Succesvol";
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(message, caption, buttons, icon);
+            }
+            catch (Exception e)
+            {
+                string message = "Er heeft zich een fout voorgedaan tijdens het opslaan: " + e.Message;
+                string caption = "Fout";
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBox.Show(message, caption, buttons, icon);
+            }
         }
 
         private void RemoveRecipe()
